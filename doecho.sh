6db570fb3458c317956174202690819bcee647c0
Andy Hermann <Andy.Hermann@ottogroup.com>
2020-02-25 12:19:13 +0100

    should now update

--- doecho.sh
+++ doecho.sh
@@ -1,4 +1,2 @@
 #!/bin/bash
-. <(xargs -0 bash -c 'printf "export %q\n" "$@"' -- < /proc/1/environ)
 echo $(date) "should be done" >> /root/varnish/varnish.log.log
-echo $VARNISH_LOG_WDIR "is the log wdir" >> /root/varnish/varnish.log.log
