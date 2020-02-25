305c17cd6ceb8dddb5b5a467bb2c916c84626485
Andy Hermann <Andy.Hermann@ottogroup.com>
2020-02-25 11:53:55 +0100

    das war richtig, jetzt muss es noch gelößt werden

--- doecho.sh
+++ doecho.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
+env - `cat /root/varnish/env.sh` /bin/sh
 echo $(date) "should be done" >> /root/varnish/varnish.log.log
 echo $VARNISH_LOG_WDIR "is the log wdir" >> /root/varnish/varnish.log.log
